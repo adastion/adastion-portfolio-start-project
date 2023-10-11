dd000d1f396eb7c92a2c57208b33fb5b2d7e1231
Andrey Sery <andrey.72006@gmail.com>
2023-10-11 22:17:06 +0300

    optimization images

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css';
 import styled from 'styled-components';
+import icons from './assets/images/icons-sprite.svg'; 
 
+const logo: string = 'square';
 
 function App() {
     return (
         <div className="App">
             <Title>Welcome to IT-INCUBATOR</Title>
+
+            <svg fill='lightblue' width='200' height='200'>
+                <use xlinkHref={`${icons}#${logo}`} />
+            </svg>
         </div>
     );
 }
