17b17b2361d856ce8619a1622b3868b4e666a4b2
Andrey Sery <andrey.72006@gmail.com>
2023-10-24 00:45:12 +0300

    CardRecommendation

--- src/layout/section/recommendations/card_recommendation/CardRecommendation.tsx
+++ src/layout/section/recommendations/card_recommendation/CardRecommendation.tsx
@@ -1,80 +1,86 @@
-import React from "react";
-import { FlexWrapper } from "../../../../components/FlexWrapper";
-import { Icon } from "../../../../components/Icons/Icon";
-import { Text } from "./../../../../components/Text";
-import { User } from "./../../../../components/user/User";
-import avatrPreview1 from "./../../../../assets/images/ava_1.jpg";
-import avatrPreview2 from "./../../../../assets/images/ava_2.jpg";
-import avatrPreview3 from "./../../../../assets/images/ava_3.jpg";
-import styled from "styled-components";
-import { theme } from "../../../../styles/Theme";
-import { SectionTitle } from "../../../../components/SectionTitle";
+import React from 'react'
+import { FlexWrapper } from '../../../../components/FlexWrapper'
+import { Icon } from '../../../../components/Icons/Icon'
+import { Text } from './../../../../components/Text'
+import avatrPreview1 from './../../../../assets/images/ava_1.jpg'
+import avatrPreview1Webp from './../../../../assets/images/ava_1.webp'
+import avatrPreview2 from './../../../../assets/images/ava_2.jpg'
+import avatrPreview2Webp from './../../../../assets/images/ava_2.webp'
+import avatrPreview3 from './../../../../assets/images/ava_3.jpg'
+import avatrPreview3Webp from './../../../../assets/images/ava_3.webp'
+import styled from 'styled-components'
+import { theme } from '../../../../styles/Theme'
+import { SectionTitle } from '../../../../components/SectionTitle'
+import { Picture } from '../../../../components/Picture'
 
 type CardRecommendationPropdType = {
-  nameIcon: string;
-  title: string;
-  description: string;
-  indexCard: number;
-  user?: {
-    avatar: string;
-    name: string;
-    profession: string;
-  };
-};
+  nameIcon: string
+  title: string
+  description: string
+  indexCard: number
+}
 
 const listUsers = [
   {
-    picture: avatrPreview1,
-    name: "James Gouse",
-    profession: "Graphic Designer",
+    picture: [avatrPreview1, avatrPreview1Webp],
+    name: 'James Gouse',
+    profession: 'Graphic Designer',
   },
   {
-    picture: avatrPreview2,
-    name: "Tiana Philips",
-    profession: "Photographer",
+    picture: [avatrPreview2, avatrPreview2Webp],
+    name: 'Tiana Philips',
+    profession: 'Photographer',
   },
   {
-    picture: avatrPreview3,
-    name: "Talan Westervelt",
-    profession: "Business man",
+    picture: [avatrPreview3, avatrPreview3Webp],
+    name: 'Talan Westervelt',
+    profession: 'Business man',
   },
-];
+]
 
 export const CardRecommendation = (props: CardRecommendationPropdType) => {
   return (
     <CardRecommendationStyled>
       <Rtating>
-        <Icon iconId={props.nameIcon} size={"18px"} />
-        <Icon iconId={props.nameIcon} size={"18px"} />
-        <Icon iconId={props.nameIcon} size={"18px"} />
-        <Icon iconId={props.nameIcon} size={"18px"} />
-        <Icon iconId={props.nameIcon} size={"18px"} />
+        <Icon iconId={props.nameIcon} fill={theme.colors.secondaryBg} size={'18px'} />
+        <Icon iconId={props.nameIcon} fill={theme.colors.secondaryBg} size={'18px'} />
+        <Icon iconId={props.nameIcon} fill={theme.colors.secondaryBg} size={'18px'} />
+        <Icon iconId={props.nameIcon} fill={theme.colors.secondaryBg} size={'18px'} />
+        <Icon iconId={props.nameIcon} fill={theme.colors.secondaryBg} size={'18px'} />
       </Rtating>
-      <SectionTitle size={"18px"} weight={"500"}>
-        Great Quality!
+      <SectionTitle as={'h4'} margin={'18px'} size={'18px'} weight={'500'}>
+        {props.title}
       </SectionTitle>
-      <Text position={"start"}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla
-        diam in ac dictum a urna viverra morbi. Morbi donec amet....
+      <Text margin={'30px'} position={'start'}>
+        {props.description}
       </Text>
-      <User
-        avatar={listUsers[props.indexCard].picture}
-        name={listUsers[props.indexCard].name}
-        profession={listUsers[props.indexCard].profession}
-      />
+      <FlexWrapper gap={'20px'}>
+        <Picture avatar width={'80px'} height={'80px'}>
+          <source srcSet={listUsers[props.indexCard].picture[1]} type='image/webp' />
+          <img src={listUsers[props.indexCard].picture[0]} alt='avatar' />
+        </Picture>
+        <FlexWrapper direction={'column'} justify={'center'}>
+          <SectionTitle as={'h4'} margin={'5px'} size={'18px'} weight={'500'}>
+            {listUsers[props.indexCard].name}
+          </SectionTitle>
+          <Text margin={'30px'} position={'start'}>
+            {listUsers[props.indexCard].profession}
+          </Text>
+        </FlexWrapper>
+      </FlexWrapper>
     </CardRecommendationStyled>
-  );
-};
+  )
+}
 
 const CardRecommendationStyled = styled.div`
   display: grid;
   max-width: 310px;
   background: ${theme.colors.primaryBg};
   padding: 25px;
-`;
+`
 
 const Rtating = styled.div`
   display: flex;
   gap: 10px;
   margin-bottom: 16px;
-`;
+`
