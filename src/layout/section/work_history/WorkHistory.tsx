0fa5d780b331c241ca7fead93037384e74cb7c09
Andrey Sery <andrey.72006@gmail.com>
2023-10-25 22:11:30 +0300

    WorkHistory

--- src/layout/section/work_history/WorkHistory.tsx
+++ src/layout/section/work_history/WorkHistory.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import styled from 'styled-components'
+import { Container } from '../../../components/Container'
 import { FlexWrapper } from '../../../components/FlexWrapper'
 import { SectionTitle } from '../../../components/SectionTitle'
+import { Table } from '../../../components/Table/Table'
 import { Text } from '../../../components/Text'
-import { Container } from '../../../components/Container'
+import { theme } from '../../../styles/Theme'
 
 export const WorkHistory = () => {
   return (
-    <FlexWrapper align={'center'} direction='column'>
+    <WorkHistoryStyled>
       <Container>
         <FlexWrapper align={'center'} direction={'column'}>
           <SectionTitle>WorkHistory</SectionTitle>
@@ -14,58 +16,42 @@ export const WorkHistory = () => {
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
             Velit officia consequat duis enim velit mollit. lorem ipsum
           </Text>
-          <table>
-            <tr>
-              <td>
-                <h4>Lead Web Designer</h4>
-                <span>Student</span>
-                <span>Jan 1016 - Dec 2021</span>
-              </td>
-              <td>
-                <h4>Certificate of web training</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus
-                  nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed
-                  amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et
-                  pellentesque. Molestie risus enim neque eget dui.
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h4>Junior Web Designer</h4>
-                <span>Student</span>
-                <span>Jan 1016 - Dec 2021</span>
-              </td>
-              <td>
-                <h4>Certificate of web training</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus
-                  nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed
-                  amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et
-                  pellentesque. Molestie risus enim neque eget dui.
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h4>Senior Web Designer</h4>
-                <span>Student</span>
-                <span>Jan 1016 - Dec 2021</span>
-              </td>
-              <td>
-                <h4>Certificate of web training</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus
-                  nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed
-                  amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et
-                  pellentesque. Molestie risus enim neque eget dui.
-                </p>
-              </td>
-            </tr>
-          </table>
+          <FlexWrapper direction={'column'} as={'ul'}>
+            <Table
+              title={'Lead Web Designer'}
+              date={'Jan 2016 - Dec 2021'}
+              infoTitle={'Certificate of web training'}
+              infoText={
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie         risus enim neque eget dui.'
+              }
+            />
+            <Table
+              title={'Junior Web Designer'}
+              date={'Jan 2016 - Dec 2021'}
+              infoTitle={'Certificate of web training'}
+              infoText={
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie         risus enim neque eget dui.'
+              }
+            />
+            <Table
+              title={'Senior Web Designer'}
+              date={'Jan 2016 - Dec 2021'}
+              infoTitle={'Certificate of web training'}
+              infoText={
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie         risus enim neque eget dui.'
+              }
+            />
+          </FlexWrapper>
         </FlexWrapper>
       </Container>
-    </FlexWrapper>
+    </WorkHistoryStyled>
   )
 }
+
+const WorkHistoryStyled = styled.div`
+  & ul {
+    padding: 42px 32px;
+    width: 100%;
+    background: ${theme.colors.primaryBg};
+  }
+`
