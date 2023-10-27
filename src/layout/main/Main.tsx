afd0219d47b42913155282b7dd08da9fe793aac0
Andrey Sery <andrey.72006@gmail.com>
2023-10-28 00:08:45 +0300

    Brands

--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -8,6 +8,10 @@ import { Portfolio } from '../section/portfolio/Portfolio'
 import { Blog } from './../section/blog/Blog'
 import { Contacts } from './../section/contacts/Contacts'
 import styled from 'styled-components'
+import { FlexWrapper } from './../../components/FlexWrapper'
+import { Icon } from '../../components/Icons/Icon'
+import { Container } from './../../components/Container'
+import { theme } from '../../styles/Theme'
 
 export const Main = () => {
   return (
@@ -20,6 +24,30 @@ export const Main = () => {
       <Portfolio />
       <Blog />
       <Contacts />
+      <Container>
+        <FlexWrapper
+          as={'ul'}
+          justify={'space-between'}
+          gap={theme.gapCard}
+          wrap={'wrap'}>
+          <li>
+            <Icon iconId={'logoPetroleum'} size={'140px'} fill={theme.colors.secondary} />
+          </li>
+          <li>
+            <Icon iconId={'logoStation'} size={'140px'} fill={theme.colors.secondary} />
+          </li>
+          <li>
+            <Icon iconId={'logoQuality'} size={'140px'} fill={theme.colors.secondary} />
+          </li>
+          <li>
+            <Icon
+              iconId={'logoStationTwo'}
+              size={'140px'}
+              fill={theme.colors.secondary}
+            />
+          </li>
+        </FlexWrapper>
+      </Container>
     </MainStyled>
   )
 }
