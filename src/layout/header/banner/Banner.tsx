f25bca9c26aa811fcfe53d97aa1b88587312749c
Andrey Sery <andrey.72006@gmail.com>
2023-10-28 23:19:26 +0300

    button "burger"

--- src/layout/header/banner/Banner.tsx
+++ src/layout/header/banner/Banner.tsx
@@ -13,6 +13,9 @@ import { Picture } from '../../../components/Picture'
 export const Banner = () => {
   return (
     <BannerStyled>
+      <button>
+        <span></span>
+      </button>
       <FlexWrapper gap={'10px'} justify={'space-between'} align={'center'}>
         <FlexWrapper direction={'column'} align={'start'} padding={'25px'}>
           <SectionTitle margin={'2px'} size={'48px'} transform>
@@ -47,6 +50,70 @@ export const Banner = () => {
 const BannerStyled = styled.section`
   background: ${theme.colors.primaryBg || '#F0F0F6'};
   color: ${theme.colors.primary};
+
+  & button {
+    position: relative;
+    top: 35px;
+    left: 30px;
+    width: 30px;
+    height: 20px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    display: none;
+
+    & span {
+      display: inline-block;
+      width: 100%;
+      height: 2px;
+      background: ${theme.colors.secondaryBg};
+    }
+
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      height: 2px;
+      width: 100%;
+      background: ${theme.colors.secondaryBg};
+      transition: all 0.1s ease-in;
+    }
+
+    &::before {
+      transform: translateY(-8px);
+    }
+
+    &::after {
+      transform: translateY(8px);
+    }
+
+    &:hover {
+      & span {
+        z-index: -1;
+      }
+
+      &::before {
+        transform: rotate(45deg) translateY(0);
+        transform-origin: center;
+      }
+
+      &::after {
+        transform: rotate(-45deg) translateY(0);
+        transform-origin: cenetr;
+      }
+    }
+
+    @media ${theme.media.bigDesktop} {
+      display: flex;
+    }
+
+    @media ${theme.media.desktop} {
+      & + * {
+        margin-top: 45px;
+      }
+    }
+  }
 
   & > ${FlexWrapper} {
     & > ${FlexWrapper} {
