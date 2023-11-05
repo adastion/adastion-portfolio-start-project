b4b3e0f72b5dda2d7672143f05670b6add24d498
Andrey Sery <andrey.72006@gmail.com>
2023-11-05 20:32:01 +0300

    useState for sideBar( open - close)

--- src/layout/sideBar/SideBar_Styles.ts
+++ src/layout/sideBar/SideBar_Styles.ts
@@ -31,7 +31,7 @@ const Profile = styled.aside<ProfilePropsType>`
         left: 0;
         right: 0;
         bottom: 0;
-        z-index: 5;
+        z-index: 30;
         backdrop-filter: brightness(0.5);
       }
     `}
